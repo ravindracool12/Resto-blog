9fd7c78349294ce8ec883eccbcd541d5ffee5cab
Ravindra <ravindry11@gmail.com>
2021-04-10 09:17:51 +0530

    my first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { BlogpostFeaturedComponent } from './blogpost/blogpost-featured/blogpost-featured.component';
+import { BlogpostListComponent } from './blogpost/blogpost-list/blogpost-list.component';
+import { BlogpostDetailComponent } from './blogpost/blogpost-detail/blogpost-detail.component';
+import { BlogpostRecentComponent } from './blogpost/blogpost-recent/blogpost-recent.component';
+import { CategoriesComponent } from './blogpost/categories/categories.component';
+import { CmspageModule } from './cmspage/cmspage.module';
+import { BlogpostModule } from './blogpost/blogpost.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    PageNotFoundComponent,
+    BlogpostFeaturedComponent,
+    BlogpostListComponent,
+    BlogpostDetailComponent,
+    BlogpostRecentComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
+    BlogpostModule,
+    CmspageModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
